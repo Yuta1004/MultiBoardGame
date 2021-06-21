3713eb7f52b35db5dbfa30d71ff1d788539042f4
NakagamiYuta <guguru0014@gmail.com>
2021-06-22 03:56:55 +0900

    [udpate] 分かりにくかったところにラベルを追加

--- starter.py
+++ starter.py
@@ -33,45 +33,48 @@ class Starter(tk.Frame):
         frame_room_create.grid(row=0, column=0, padx=8, pady=8, sticky=tk.W+tk.E+tk.N+tk.S)
 
         ### 実行可能ゲームのリスト
+        ttk.Label(frame_room_create, text="Games").grid(row=0, column=0, padx=5, pady=1, stick=tk.W)
         self.available_game_list = tk.Listbox(frame_room_create, height=7)
         self.available_game_list.insert(tk.END, "Game A")
         self.available_game_list.insert(tk.END, "Game B")
         self.available_game_list.insert(tk.END, "Game C")
         self.available_game_list.insert(tk.END, "Game D")
         self.available_game_list.select_set(0)
-        self.available_game_list.grid(row=0, column=0, padx=5, pady=5)
+        self.available_game_list.grid(row=1, column=0, padx=5, pady=5)
 
         ### 部屋名入力欄
+        ttk.Label(frame_room_create, text="Room Name").grid(row=2, column=0, padx=5, pady=1, stick=tk.W)
         self.entry_room_create = ttk.Entry(frame_room_create)
-        self.entry_room_create.insert(0, "RoomName")
-        self.entry_room_create.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_room_create.insert(0, "aaaaaaaaaaaaa")
+        self.entry_room_create.grid(row=3, column=0, padx=5, pady=5)
 
         ### 部屋作成ボタン
         button_room_create = ttk.Button(frame_room_create)
         button_room_create.configure(text="Create")
-        button_room_create.grid(row=2, column=0, padx=5, pady=5, stick=tk.W+tk.E)
+        button_room_create.grid(row=4, column=0, padx=5, pady=5, stick=tk.W+tk.E)
 
         ## 部屋"参加"フレーム
         frame_room_join = ttk.LabelFrame(frame_room, text="Join Room")
         frame_room_join.grid(row=0, column=1, padx=8, pady=8, stick=tk.W+tk.E+tk.N+tk.S)
 
         ### 参加可能な部屋のリスト
+        ttk.Label(frame_room_join, text="Available Rooms").grid(row=0, column=0, padx=5, pady=1, stick=tk.W)
         self.available_room_list = tk.Listbox(frame_room_join, height=7)
         self.available_room_list.insert(tk.END, "Room1 (KoiKoi)")
         self.available_room_list.insert(tk.END, "Room2 (Sevens)")
         self.available_room_list.insert(tk.END, "Room3 (Old Maid)")
         self.available_room_list.select_set(0)
-        self.available_room_list.grid(row=0, column=0, columnspan=1, padx=5, pady=5)
+        self.available_room_list.grid(row=1, column=0, columnspan=1, padx=5, pady=5)
 
         ### 部屋参加ボタン
         button_room_join = ttk.Button(frame_room_join)
         button_room_join.configure(text="Update")
-        button_room_join.grid(row=1, column=0, padx=5, pady=5, stick=tk.W+tk.E)
+        button_room_join.grid(row=2, column=0, padx=5, pady=5, stick=tk.W+tk.E)
 
         ### 部屋リスト更新ボタン
         button_room_update = ttk.Button(frame_room_join)
         button_room_update.configure(text="Join")
-        button_room_update.grid(row=2, column=0, padx=5, pady=5, stick=tk.W+tk.E)
+        button_room_update.grid(row=3, column=0, padx=5, pady=5, stick=tk.W+tk.E)
 
         # 設定フレーム
         frame_settings = ttk.LabelFrame(self, text="Settings")
