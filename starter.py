313d50314f06350f74fae40e8fe432bf23615169
NakagamiYuta <guguru0014@gmail.com>
2021-06-22 03:39:52 +0900

    [add] 設定UIをいい感じに配置

--- starter.py
+++ starter.py
@@ -74,6 +74,41 @@ class Starter(tk.Frame):
         self.button_room_update.grid(row=2, column=0, padx=5, pady=5, stick=tk.W+tk.E)
 
         # 設定フレーム
+        self.frame_settings = ttk.LabelFrame(self, text="Settings")
+        self.frame_settings.grid(row=0, column=1, padx=10, pady=10, stick=tk.W+tk.E+tk.N+tk.S)
+
+        ## インタフェース表示
+        self.label_interfaces = ttk.Label(self.frame_settings, text="Interfaces")
+        self.label_interfaces.grid(row=0, column=0, padx=5, pady=1, stick=tk.W)
+        self.text_interfaces = tk.Text(self.frame_settings, width=40, height=5)
+        self.text_interfaces.insert("1.0", "xxx.xxx.xxx.xxx / xxx.xxx.xxx.xxx\n"*4)
+        self.text_interfaces.configure(state=tk.DISABLED)
+        self.text_interfaces.grid(row=1, column=0, padx=5, pady=5)
+
+        ## パスワード入力欄
+        self.label_password = ttk.Label(self.frame_settings, text="Password")
+        self.label_password.grid(row=2, column=0, padx=5, pady=1, stick=tk.W)
+        self.entry_password = ttk.Entry(self.frame_settings)
+        self.entry_password.insert(0, "aaaaa")
+        self.entry_password.grid(row=3, column=0, padx=5, pady=5, stick=tk.W+tk.E)
+
+        ## ポート入力フレーム
+        self.frame_port = ttk.LabelFrame(self.frame_settings, text="Port")
+        self.frame_port.grid(row=4, column=0, padx=8, pady=8, stick=tk.W+tk.E+tk.N+tk.S)
+
+        ### ポート入力欄(TCP)
+        self.label_port_tcp = ttk.Label(self.frame_port, text="TCP: ")
+        self.label_port_tcp.grid(row=0, column=0, padx=1, pady=1)
+        self.entry_port_tcp = ttk.Entry(self.frame_port, width=8)
+        self.entry_port_tcp.insert(0, 50000)
+        self.entry_port_tcp.grid(row=0, column=1, padx=1, pady=1)
+
+        ### ポート入力欄(UDP)
+        self.label_port_udp = ttk.Label(self.frame_port, text="UDP: ")
+        self.label_port_udp.grid(row=0, column=3, padx=1, pady=1)
+        self.entry_port_udp = ttk.Entry(self.frame_port, width=8)
+        self.entry_port_udp.insert(0, 50001)
+        self.entry_port_udp.grid(row=0, column=4, padx=1, pady=1)
 
         # ログフレーム
 
