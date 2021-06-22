dfd6c75612d1b67ca77b51f17dc687b31e055882
NakagamiYuta <guguru0014@gmail.com>
2021-06-23 03:30:00 +0900

    [add] マンカラを遊べるようにした

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import tkinter as tk
 
 from launcher import Launcher
 from game.example.main import Example
+from game.mancala.main import Mancala
 
 games = {
-    "Example": (Example, 2)      # "ゲーム名" : (ゲーム実行可能クラス, 人数上限)
+    # "Example": (Example, 2)      # "ゲーム名" : (ゲーム実行可能クラス, 人数上限)
+    "Mancala": (Mancala, 2)
 }
 
 launcher = Launcher(tk.Tk(), games)
