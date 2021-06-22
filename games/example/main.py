d1b2a36d9128c8fb0b5f9d8aeba43474b94aedba
NakagamiYuta <guguru0014@gmail.com>
2021-06-22 21:50:11 +0900

    [add] base.pyのUsageに書いているコード例をexample/main.pyに書いた

---
+++ games/example/main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+from games.base import GameBase
+
+
+class Example(GameBase):
+
+    def __init__(self, master, room_mgr, port_udp):
+        super().__init__(master, room_mgr, port_udp, title="Empty", width=700, height=400)
+
+    def setup_widgets(self):
+        ttk.Label(self, text="Example").grid(row=0, column=0)
+        self.text_viewer = tk.Text(self, width=20, height=10)
+        self.text_viewer.grid(row=1, column=0)
+
+    def update(self):
+        self.text_viewer.insert("1.0", "Updated\n")
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
 from starter import Starter
-from games.template.main import Template
+from games.example.main import Example
 
 games = {
-    "Empty": (Template, 4)      # "ゲーム名" : (ゲーム実行可能クラス, 人数上限)
+    "Example": (Example, 2)      # "ゲーム名" : (ゲーム実行可能クラス, 人数上限)
 }
 
 starter = Starter(tk.Tk(), games)
