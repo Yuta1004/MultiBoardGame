91682f7510f0024b19f090c53cd66dd48978562b
NakagamiYuta <guguru0014@gmail.com>
2021-06-22 23:22:30 +0900

    [add] ゲーム開始判定→メッセージ表示まで実装

--- game/mancala/main.py
+++ game/mancala/main.py
@@ -1,7 +1,13 @@
+import random
+
 import tkinter as tk
 from tkinter import ttk
 
 from game.base import GameBase
+
+
+HOST = 0
+CLIENT = 1
 
 
 class Mancala(GameBase):
@@ -14,12 +20,24 @@ class Mancala(GameBase):
         self.canvas.pack()
 
     def update(self):
+        # プレイヤーが揃ったらゲーム開始
+        if self.is_host():
+            if self.room_mgr.get_value("turn") is None and len(self.room_mgr.user_list) == 1:
+                self.room_mgr.set_values(
+                    turn=random.randint(0, 1),       ## Host=>0, Client=>1
+                    board=[4, 4, 4, 0, 4, 4, 4, 0]     ## ホストの陣地の左端が0
+                )
+                self.room_mgr.sync()
+
         self.draw()
 
     def draw(self):
         """
         主にCanvasの描画を行う
         """
+        # 背景
+        self.canvas.create_rectangle(0, 0, 1100, 600, fill="white")
+
         # フィールド
         for i in range(3):
             x = 225 + i*250
@@ -28,6 +46,15 @@ class Mancala(GameBase):
         self.canvas.create_rectangle(40, 225, 50+150, 225+150, width=3, fill="white", dash=(10, 10))
         self.canvas.create_rectangle(910, 225, 910+150, 225+150, width=3, fill="white", dash=(10, 10))
 
+        # 指示内容決定
+        msg = ""
+        if (self.is_host() and self.room_mgr.get_value("turn") == HOST) or (not self.is_host() and self.room_mgr.get_value("turn") == CLIENT):
+            msg = "Your turn"
+        elif self.room_mgr.get_value("turn") is None:
+            msg = "Waiting players..."
+        else:
+            msg =  "Thinking..."
+
         # 指示位置
         self.canvas.create_line(250, 330, 850, 330)
-        self.canvas.create_text(550, 300, text="Waiting players...", font=("Menlo", 40), tag="msg_box")
+        self.canvas.create_text(550, 300, text=msg, font=("Menlo", 40))
