77f82c11faeb5a2e9632685278ec75f855dd9996
NakagamiYuta <guguru0014@gmail.com>
2021-07-14 00:16:00 +0900

    [add] 大体のUIを整えた

--- game/koikoi/main.py
+++ game/koikoi/main.py
@@ -1,5 +1,8 @@
+import random
+
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk, ImageEnhance
 
 from game.base import GameBase
 
@@ -7,10 +10,46 @@ from game.base import GameBase
 class KoiKoi(GameBase):
 
     def __init__(self, master, room_mgr, port_udp):
-        super().__init__(master, room_mgr, port_udp, title="KoiKoi", width=700, height=400)
+        super().__init__(master, room_mgr, port_udp, title="KoiKoi", width=1200, height=700)
+        self.load_resources()
+        self.draw()
+
+    def load_resources(self):
+        # 背景
+        bg_img = Image.open(open("game/koikoi/resource/bg.png", "rb"))
+        bg_img = bg_img.resize((1200, 700))
+        bg_img = ImageEnhance.Brightness(bg_img).enhance(0.7)
+        self.bg_img = ImageTk.PhotoImage(bg_img)
+
+        # 合い札置き場
+        aihuda_img = Image.open(open("game/koikoi/resource/aihuda.png", "rb"))
+        aihuda_img = aihuda_img.resize((280, 700))
+        self.aihuda_img = ImageTk.PhotoImage(aihuda_img)
+
+        # 札(裏)
+        back_card_img = Image.open(open("game/koikoi/resource/card/back.png", "rb"))
+        back_card_img = back_card_img.resize((90, 120))
+        self.back_card_img = ImageTk.PhotoImage(back_card_img)
 
     def setup_widgets(self):
-        ttk.Label(self, text="KoiKoi").grid(row=0, column=0)
+        self.canvas = tk.Canvas(self, width=1200, height=700, bg="white")
+        self.canvas.pack()
 
     def update(self):
         pass
+
+    def draw(self):
+        # リセット
+        self.canvas.delete(tk.ALL)
+
+        # 背景画像
+        self.canvas.create_image(0, 0, image=self.bg_img, anchor=tk.NW)
+
+        # 合い札置き場
+        self.canvas.create_image(0, 0, image=self.aihuda_img, anchor=tk.NW)
+        self.canvas.create_image(920, 0, image=self.aihuda_img, anchor=tk.NW)
+        self.canvas.create_line(0, 400, 280, 400, width=3, fill="black")
+        self.canvas.create_line(920, 300, 1200, 300, width=3, fill="black")
+
+        # 山札
+        self.canvas.create_image(600, 350, image=self.back_card_img, anchor=tk.CENTER)
