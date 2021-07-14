07c0a7f786c896900696283f7a0897786983f934
NakagamiYuta <guguru0014@gmail.com>
2021-07-15 00:12:42 +0900

    [change] 役名を英語表記にした

--- game/koikoi/card/calc.py
+++ game/koikoi/card/calc.py
@@ -2,18 +2,19 @@ from enum import Enum
 
 
 class Role(Enum):
-    Goko = 0                    # 五光
-    Shiko = 1                    # 四光
-    Sanko = 2                   # 三光
-    HanamiDeIppai = 3      # 花見で一杯
-    TsukimiDeIppai = 4      # 月見で一杯
-    InoShikaCho = 5         #  猪鹿蝶
-    Akatan = 6                 # 赤短
-    Aotan = 7                   # 青短
-    AkatanAndAotan = 8  # 赤単と青短の重複
-    Tane = 9                     # タネ
-    Tan = 10                     # タン
-    Kasu = 11                   # カス
+    FiveLights = 0                       # 五光
+    FourLights = 1                      # 四光
+    FourLights_with_Willow =2    # 雨四光
+    ThreeLights = 3                    # 三光
+    Hanami_de_Ippai = 3             # 花見で一杯
+    Tsukimi_de_Ippai = 4             # 月見で一杯
+    Boar_Deer_Butterfly = 5       # 猪鹿蝶
+    BlueSlips = 6                        # 青タン
+    RedSlips = 7                         # 赤タン
+    Blue_Red_Slips = 8               # 青タンと赤タンの重複
+    Animals = 9                          # タネ
+    Slips = 10                             # タン
+    Drags = 11                           # カス
 
 
 def calc_score(collected_card):
@@ -35,62 +36,62 @@ def calc_score(collected_card):
 
     if check_enable_bit_nums(collected_card, 0xf80000)  ==  5:       # 五光
         score += 10
-        roles.append((Role.Goko, 10))
+        roles.append((Role.FiveLights, 10))
     elif check_enable_bit_nums(collected_card, 0xf80000) == 4:      # 四光
         if collected_card & 0x800000 > 0:
             score += 7
-            roles.append((Role.Shiko, 7))
+            roles.append((Role.FourLights_with_Willow, 7))
         else:
             score += 8
-            roles.append((Role.Shiko), 8)
+            roles.append((Role.FourLights, 8))
     elif check_enable_bit_nums(collected_card, 0x380000) == 3:      # 三光
         score += 5
-        roles.append((Role.Sanko, 5))
+        roles.append((Role.ThreeLights, 5))
 
     # 花見で一杯
     if check_enable_bit_nums(collected_card, 0x88000) == 2:
         score += 5
-        roles.append((Role.HanamiDeIppai, 5))
+        roles.append((Role.Hanami_de_Ippai, 5))
 
     # 月見で一杯
     if check_enable_bit_nums(collected_card, 0x108000) == 2:
         score += 5
-        roles.append((Role.TsukimiDeIppai, 5))
+        roles.append((Role.Tsukimi_de_Ippai, 5))
 
     # 猪鹿蝶
     if check_enable_bit_nums(collected_card, 0x70000) == 3:
         surplus = check_enable_bit_nums(collected_card, 0xfc00)
         score += 5 + surplus
-        roles.append((Role.InoShikaCho, 5+surplus))
+        roles.append((Role.Boar_Deer_Butterfly, 5+surplus))
 
     if check_enable_bit_nums(collected_card, 0x3f0) == 6:       # 赤短と青短の重複
         surplus = check_enable_bit_nums(collected_card, 0xf)
         score += 10 + surplus
-        roles.append((Role.AkatanAndAotan, 10+surplus))
+        roles.append((Role.Blue_Red_Slips, 10+surplus))
     elif check_enable_bit_nums(collected_card, 0x70) == 3:      # 赤短
         surplus = check_enable_bit_nums(0x38f)
         score += 5 + surplus
-        roles.append((Role.Akatan, 5+surplus))
+        roles.append((Role.RedSlips, 5+surplus))
     elif check_enable_bit_nums(collected_card, 0x380) == 3:     # 青短
         surplus = check_enable_bit_nums(0x7f)
         score += 5 + surplus
-        roles.append((Role.Aotan, 5+surplus))
+        roles.append((Role.BlueSlips, 5+surplus))
     elif check_enable_bit_nums(collected_card, 0x3ff) >= 5:     # タン
         surplus = check_enable_bit_nums(collected_card, 0x3ff)
         score += 1 + surplus - 5
-        roles.append((Role.Tan, 1+surplus-5))
+        roles.append((Role.Slips, 1+surplus-5))
 
     # タネ
     if check_enable_bit_nums(collected_card, 0x7fc00) > 5:
         surplus = check_enable_bit_nums(collected_card, 0x7fc00)
         score += 1 + surplus - 5
-        roles.append((Role.Tane, 1+surplus-5))
+        roles.append((Role.Animals, 1+surplus-5))
 
     # カス
     if  check_enable_bit_nums(collected_card, 0xffffff008000) > 10:
         surplus = check_enable_bit_nums(collected_card, 0xffffff008000)
         score += 1 + surplus - 10
-        roles.append((Role.Kasu, 1+surplus-10))
+        roles.append((Role.Drags, 1+surplus-10))
 
     return score, roles
 
